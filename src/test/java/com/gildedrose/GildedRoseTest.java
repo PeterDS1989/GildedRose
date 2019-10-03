4d90fb94ebc2597e54ab0c88befab04885e3a554
Peter De smedt <peter.desmedt@external.engie.com>
2019-10-03 10:20:09 +0200

    Comment UT

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,18 +12,22 @@ public class GildedRoseTest {
         Item[] items = new Item[] {item};
         GildedRose app = new GildedRose(items);
 
+        //Minus one for sell date not passed
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(0);
         assertThat(item.quality).isEqualTo(2);
 
+        //Minus two for sell date passed
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(-1);
         assertThat(item.quality).isEqualTo(0);
 
+        //Minimum of 0 quality
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(-2);
         assertThat(item.quality).isEqualTo(0);
 
+        //Max limit of 50 does not apply when downgrading quality
         item.quality = 60;
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(-3);
@@ -36,14 +40,17 @@ public class GildedRoseTest {
         Item[] items = new Item[] {item};
         GildedRose app = new GildedRose(items);
 
+        //Quality increases by 1 if sell date not passed
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(0);
         assertThat(item.quality).isEqualTo(48);
 
+        //increases by 2 if sell date passed
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(-1);
         assertThat(item.quality).isEqualTo(50);
 
+        //Max 50 quality
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(-2);
         assertThat(item.quality).isEqualTo(50);
@@ -51,6 +58,7 @@ public class GildedRoseTest {
         item.sellIn = 0;
         item.quality = 49;
 
+        //Max 50 quality
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(-1);
         assertThat(item.quality).isEqualTo(50);
@@ -62,16 +70,19 @@ public class GildedRoseTest {
         Item[] items = new Item[] {item};
         GildedRose app = new GildedRose(items);
 
+        //Unchanged
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(1);
         assertThat(item.quality).isEqualTo(47);
 
+        //Unchanged
         item.quality = -5;
         item.sellIn = -1;
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(-1);
         assertThat(item.quality).isEqualTo(-5);
 
+        //Unchanged
         item.quality = 80;
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(-1);
@@ -84,19 +95,23 @@ public class GildedRoseTest {
         Item[] items = new Item[] {item};
         GildedRose app = new GildedRose(items);
 
+        //+1 quality if sell date > 10
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(10);
         assertThat(item.quality).isEqualTo(45);
 
+        //+2 quality if 6 <= sell date <= 10
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(9);
         assertThat(item.quality).isEqualTo(47);
 
+        //+2 quality if 6 <= sell date <= 10
         item.sellIn = 6;
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(5);
         assertThat(item.quality).isEqualTo(49);
 
+        //+3 quality if 1 <= sell date <= 5, max 50
         item.quality = 44;
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(4);
@@ -118,6 +133,7 @@ public class GildedRoseTest {
         assertThat(item.sellIn).isEqualTo(0);
         assertThat(item.quality).isEqualTo(50);
 
+        //0 quality when sell date passed
         app.updateQuality();
         assertThat(item.sellIn).isEqualTo(-1);
         assertThat(item.quality).isEqualTo(0);
