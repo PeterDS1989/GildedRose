8a30c454d53fd7d349b96a0828d70757bba5dfc1
Peter De smedt <peter.desmedt@external.engie.com>
2019-10-03 09:51:23 +0200

    Steps in README UT existing functionality

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # GildedRose
+
+Steps:
+- GildedRoseTest adapted to test existing functionality
+- Change existing function updateQuality
+- GildedRoseTest adapted to add test for new functionality
+- Add code for new functionality
+
+Pom file contains maven compiler version and additional UT library.
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,129 @@ package com.gildedrose;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.fest.assertions.Assertions.assertThat;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void testNormalItems() {
+        Item item = new Item("foo", 1, 3);
+        Item[] items = new Item[] {item};
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertThat(item.sellIn).isEqualTo(0);
+        assertThat(item.quality).isEqualTo(2);
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(-1);
+        assertThat(item.quality).isEqualTo(0);
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(-2);
+        assertThat(item.quality).isEqualTo(0);
+
+        item.quality = 60;
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(-3);
+        assertThat(item.quality).isEqualTo(58);
+    }
+
+    @Test
+    public void testAgedBrie() {
+        Item item = new Item("Aged Brie", 1, 47);
+        Item[] items = new Item[] {item};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(0);
+        assertThat(item.quality).isEqualTo(48);
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(-1);
+        assertThat(item.quality).isEqualTo(50);
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(-2);
+        assertThat(item.quality).isEqualTo(50);
+
+        item.sellIn = 0;
+        item.quality = 49;
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(-1);
+        assertThat(item.quality).isEqualTo(50);
+    }
+
+    @Test
+    public void testSulfuras() {
+        Item item = new Item("Sulfuras, Hand of Ragnaros", 1, 47);
+        Item[] items = new Item[] {item};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(1);
+        assertThat(item.quality).isEqualTo(47);
+
+        item.quality = -5;
+        item.sellIn = -1;
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(-1);
+        assertThat(item.quality).isEqualTo(-5);
+
+        item.quality = 80;
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(-1);
+        assertThat(item.quality).isEqualTo(80);
+    }
+
+    @Test
+    public void testBackstagePasses() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 44);
+        Item[] items = new Item[] {item};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(10);
+        assertThat(item.quality).isEqualTo(45);
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(9);
+        assertThat(item.quality).isEqualTo(47);
+
+        item.sellIn = 6;
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(5);
+        assertThat(item.quality).isEqualTo(49);
+
+        item.quality = 44;
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(4);
+        assertThat(item.quality).isEqualTo(47);
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(3);
+        assertThat(item.quality).isEqualTo(50);
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(2);
+        assertThat(item.quality).isEqualTo(50);
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(1);
+        assertThat(item.quality).isEqualTo(50);
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(0);
+        assertThat(item.quality).isEqualTo(50);
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(-1);
+        assertThat(item.quality).isEqualTo(0);
+
+        app.updateQuality();
+        assertThat(item.sellIn).isEqualTo(-2);
+        assertThat(item.quality).isEqualTo(0);
     }
 
 }
